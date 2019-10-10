0aeaa57f03bb88f01e2bfc2cd73db183bc0d054e
sovio <56193362+sovio@users.noreply.github.com>
2019-10-10 19:18:33 +0200

    Update restor.php
    naprawienie buga "możliwość zmiany hasla każdemu"

--- login/restor/restor.php
+++ login/restor/restor.php
@@ -4,7 +4,8 @@
         $haslo = password_hash($_POST['haslo2'] , PASSWORD_BCRYPT );
         require_once "../connect.php"; 
         $polaczenie = @new mysqli($host,$db_user,$db_password,$db_name);
-        $email = $_POST['email2'];
+        if($_SESSION['email2'] != $_POST['email2']){echo 'podales zly emial!';exit;}
+        $email = $_SESSION['email2'];
         $query = "SELECT id FROM logowanie WHERE email='$email' AND mailconfimed=1";
         $sql = $polaczenie->query($query);
         $sql1 = $sql->fetch_assoc();
