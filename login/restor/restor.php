c9ca5ee2bc62520bde7ba71fdb5bb7d4a4e5887c
sovio <56193362+sovio@users.noreply.github.com>
2019-10-11 15:42:27 +0200

    Update restor.php
    naprawa bugu

--- login/restor/restor.php
+++ login/restor/restor.php
@@ -4,7 +4,13 @@
         $haslo = password_hash($_POST['haslo2'] , PASSWORD_BCRYPT );
         require_once "../connect.php"; 
         $polaczenie = @new mysqli($host,$db_user,$db_password,$db_name);
+        $email1 = $_POST['email2'];
+          $sql2 = "SELECT*FROM logowanie WHERE email='$email1'";
+          $sql2 = $polaczenie->query($sql2);
+          $sql2 = $sql2->fetch_assoc();
+          $token = $sql2['token'];
         if($_SESSION['email2'] != $_POST['email2']){echo 'podales zly emial!';exit;}
+        if($token != $_SESSION['token2']){echo 'podales zly emial!';exit;}
         $email = $_SESSION['email2'];
         $query = "SELECT id FROM logowanie WHERE email='$email' AND mailconfimed=1";
         $sql = $polaczenie->query($query);
