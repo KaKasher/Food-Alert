843aaf66f0dce60a961ba7d2e0a8ff5f79fbaff9
Jacobb536 <jakub.wojdak16@gmail.com>
2019-10-17 21:50:11 +0200

    Update index.php
    Wysuwany modal z lewej do usuwania znaczników

--- index.php
+++ index.php
@@ -55,8 +55,11 @@
 
 <?php
  	if(isset($_SESSION['zalogowany'])){
-       	echo '<div class="add"><button class="btn btn-default" type="button" data-toggle="modal" data-target="#popup">Dodaj</button></div>';}
+           echo '<div class="add"><button class="btn btn-default" type="button" data-toggle="modal" data-target="#popup">Dodaj</button></div>
+                 <div class="add"><button class="btn btn-light" type="button" data-toggle="modal" data-target="#znaczniki"><i class="fas fa-map-marker-alt"></i>Znaczniki</button></div>';}
        ?>
+
+     
 
 
  <!------------------------------------------------------------------------------------------------------------------POPUP DODAWANIA JEDZENIA------------------------------------------------------------------------------------------------------------------------------------------------------->
@@ -84,14 +87,32 @@
                 </div>
 
         </div>
-
-
+<!------------------------------------------------------------------------------------------------------------------POPUP USUWANIA ZNACZNIKÓW------------------------------------------------------------------------------------------------------------------------------------------------------->
+                <div class="modal left fade" id="znaczniki" tabindex="-1" role="dialog">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Znaczniki</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        I tutaj będą te znaczniki
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Zamknij</button>
+                        <button type="button" class="btn btn-default" type="submit">Zapisz</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
 <!------------------------------------------------------------------------------------------------------------------SKRYPTY------------------------------------------------------------------------------------------------------------------------------------------------------->
 
         <script src="mapa/map.js"></script>
         <script src="https://maps.googleapis.com/maps/api/js?key=&libraries=places&language=pl&region=PL&callback=initMap"
         async defer></script>
-
+        <script src="https://kit.fontawesome.com/7dbd9042fb.js" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
