503bdc6e0cde28a6849a75d2276e59400b977459
Jacobb536 <jakub.wojdak16@gmail.com>
2019-10-17 19:12:56 +0200

    Update Kontakt.php

--- kontakt/Kontakt.php
+++ kontakt/Kontakt.php
@@ -1,107 +1,110 @@
 ﻿<?php
     session_start();
 ?>
-<!DOCTYPE html>
-<head>
 
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="KontaktStyle.css">
-    <link rel="stylesheet" href="../NavbarMainStyle.css"  type="text/css"/>
-    <link rel="stylesheet" href="../NavbarMainStyleAButton.css"  type="text/css"/>
-</head>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="../NavbarMainStyle.css"  type="text/css"/>
+        <link rel="stylesheet" href="../NavbarMainStyleAButton.css"  type="text/css"/>
+        <link rel="stylesheet" type="text/css" href="KontaktStyle.css">
+        <title>Food Alert</title>
+    </head>
 
-<body>
-  <header>
-    <nav class="navbar navbar-dark navbar-expand-md">
-        <a class="navbar-brand" href="../strona-glowna">Food Alert</a>
-        <button class="navbar-toggler animated-button-js" type="button" data-toggle="collapse" data-target="#menu">
-            <div class="animated-button"><span></span><span></span><span></span><span></span></div>
-        </button>
-        
-        <div class="collapse navbar-collapse" id="menu">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                <?php
+    <body>
+        <header>
+            <nav class="navbar navbar-dark navbar-expand-md">
+                <a class="navbar-brand" href="../strona-glowna">Food Alert</a>
+                <button class="navbar-toggler animated-button-js" type="button" data-toggle="collapse" data-target="#menu">
+                    <div class="animated-button"><span></span><span></span><span></span><span></span></div>
+                </button>
+                
+                <div class="collapse navbar-collapse" id="menu">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                        <?php
                         if(isset($_SESSION['zalogowany']))
                         echo '<a class="nav-link" href="../login/logout.php"> Wyloguj</a>';
                         else
                         echo '<a class="nav-link" href="../login">Zaloguj</a>';
                     ?>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../onas/Onas.php">O nas</a>
-                </li>	
-                <li class="nav-item">
-                    <a class="nav-link" href="../kontakt/Kontakt.php">Kontakt</a>
-                </li>		
-            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../onas/Onas.php">O nas</a>
+                        </li>	
+                        <li class="nav-item">
+                            <a class="nav-link" href="../kontakt/Kontakt.php">Kontakt</a>
+                        </li>		
+                    </ul>
 
-        </div>
-        
-    </nav>
-</header>
-    <main>
+                </div>
+                
+            </nav>
+        </header>
+
+        <main>
             <div class="glowny">
-                    <div class="container-fluid">
-                        <h1>Kontakt</h1>
-                      <div class="row">
-                        <div class="col-md-4"> <i class="fas fa-user fa-6x"> </i>
-                          <p>
-                          Project Manager<br>
-                          Kacper Gregorowicz<br>
-                          E-mail:<br> k.gregorowicz@gmail.com
-                        </p>
-                        </div> 
-                        <div class="col-md-4"><i class="fas fa-user fa-6x"></i>
-                          <p>
-                          Front-end developer<br>
-                          Oskar Michta<br>
-                          E-mail:<br> 
-                          michtabiznes@gmail.com
-                        </p>
-                        </div> 
-                        <div class="col-md-4"><i class="fas fa-user fa-6x"></i>
-                        <p>
-                          Web Designer/Front-end developer<br>
-                          Jakub Wojdak<br>
-                          E-mail: <br>
-                          jakub.wojdak11@gmail.com
-                          </p>
-                        </div>
-                      </div>
-                
-                
-                      <div class="row">
-                          <div class="col-md-4 offset-md-2"><i class="fas fa-user fa-6x"></i>
-                            <p>
-                            Technology Consultant<br>
-                            Eliasz Nalepka<br>
-                            E-mail:<br> 
-                            godlike.capybara@gmail.com
-                          </p>
-                          </div> 
-                        
-                          <div class="col-md-4"><i class="fas fa-user fa-6x"></i>
-                            <p>
-                            Back-end developer<br>
-                            Dawid Sroka<br>
-                            E-mail:<br> 
-                            dawidfirm1@gmail.com
-                          </p>
-                          </div> 
-                      </div>
+                <h1>Kontakt</h1>
+                    <div class="kontener">
+                        <div class="content">
+                                <div class="row">
+                                <div class="col-md-4"> <i class="fas fa-user fa-6x"> </i>
+                                <p>
+                                Project Manager<br>
+                                Kacper Gregorowicz<br>
+                                E-mail:<br> k.gregorowicz@gmail.com
+                                </p>
+                                </div> 
+                                <div class="col-md-4"><i class="fas fa-user fa-6x"></i>
+                                <p>
+                                Front-end developer<br>
+                                Oskar Michta<br>
+                                E-mail:<br> 
+                                michtabiznes@gmail.com
+                                </p>
+                                </div> 
+                                <div class="col-md-4"><i class="fas fa-user fa-6x"></i>
+                                <p>
+                                Web Designer/Front-end developer<br>
+                                Jakub Wojdak<br>
+                                E-mail: <br>
+                                jakub.wojdak11@gmail.com
+                                </p>
+                                </div>
+                            </div>
+
+                                    <div class="row">
+                                <div class="col-md-4 offset-md-2"><i class="fas fa-user fa-6x"></i>
+                                    <p>
+                                    Technology Consultant<br>
+                                    Eliasz Nalepka<br>
+                                    E-mail:<br> 
+                                    godlike.capybara@gmail.com
+                                </p>
+                                </div> 
+                                
+                                <div class="col-md-4"><i class="fas fa-user fa-6x"></i>
+                                    <p>
+                                    Back-end developer<br>
+                                    Dawid Sroka<br>
+                                    E-mail:<br> 
+                                    dawidfirm1@gmail.com
+                                </p>
+                                </div> 
+                            </div>
                     </div>
-                  </div>
-    </main>
+                </div>
+            </div>   
+    
+    
+        </main>
 
-
-    <script src="https://kit.fontawesome.com/7dbd9042fb.js" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>  
-    <script src="../Button.js"></script>
-</body>
+        <script src="https://kit.fontawesome.com/7dbd9042fb.js" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="../Button.js"></script>
+    </body>
 </html>
